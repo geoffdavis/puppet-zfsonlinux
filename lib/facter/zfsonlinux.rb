fd018e9591b85b4eef42bdfb78e681bc11c623db
Geoff Davis <gadavis@ucsd.edu>
2012-04-19 11:18:49 -0700

    Add facts to detect the version of spl and zfs
    Fact names are zfsonlinux_spl_version and zfsonlinux_zfs_version

---
+++ lib/facter/zfsonlinux.rb
@@ -0,0 +1,37 @@
+# zfsonlinux.rb
+#
+# This generates two facts on Linux systems:
+# * zfsonlinux_spl_version
+# * zfsonlinux_rpm_version
+#
+# Resources:
+#   Multi-os facts:
+# https://github.com/puppetlabs/facter/blob/master/lib/facter/operatingsystemrelease.rb
+#   Redhat package versions:
+# http://geek.jasonhancock.com/2011/11/01/package-version-as-fact-in-puppet/
+#   Debian package versions:
+# https://blog.kumina.nl/2010/03/puppet-tipstricks-getting-the-version-from-a-package/
+#
+if Facter.value(:kernel) == 'Linux'
+  case Facter.value(:operatingsystem)
+  when "CentOS", "RedHat", "Scientific", "SLC", "Ascendos",
+    "CloudLinux", "PSBM"
+    spl_version = %x{/bin/rpm -qa --queryformat "%{VERSION}-%{RELEASE}" spl}
+    zfs_version = %x{/bin/rpm -qa --queryformat "%{VERSION}-%{RELEASE}" zfs}
+  when "Debian", "Ubuntu"
+    spl_version = %x{/usr/bin/dpkg-query -W -f='${Version}' spl}
+    zfs_version = %x{/usr/bin/dpkg-query -W -f='${Version}' zfs}
+  end
+
+  Facter.add(:zfsonlinux_spl_version) do
+    setcode do
+      spl_version
+    end
+  end
+
+  Facter.add(:zfsonlinux_zfs_version) do
+    setcode do
+      zfs_version
+    end
+  end
+end
--- manifests/install/redhat.pp
+++ manifests/install/redhat.pp
@@ -4,106 +4,113 @@ class zfsonlinux::install::redhat (
   $zfs_version  = $zfsonlinux::params::zfs_version,
   $timeout      = undef
 ) inherits zfsonlinux::params {
-  require staging
 
-  ####
-  # Set up variables
-  ####
+  # No-op if we're already at the right version
+  if $::zfsonlinux_zfs_version == $zfs_version {
+    notice "zfsonlinux already at requested version $zfs_version"
+  } else {
+    require staging
 
-  # http://github.com/downloads/zfsonlinux/spl/spl-0.6.0-rc8.tar.gz
-  $spl_installer_tar = "spl-${spl_version}.tar.gz"
-  $spl_installer_dir = "spl-${spl_version}"
-  $spl_source_url = "${download_dir}/spl/${spl_installer_tar}"
+    ####
+    # Set up variables
+    ####
 
-  $zfs_installer_tar = "zfs-${zfs_version}.tar.gz"
-  $zfs_installer_dir = "zfs-${zfs_version}"
-  $zfs_source_url = "${download_dir}/zfs/${zfs_installer_tar}"
+    # http://github.com/downloads/zfsonlinux/spl/spl-0.6.0-rc8.tar.gz
+    $spl_installer_tar = "spl-${spl_version}.tar.gz"
+    $spl_installer_dir = "spl-${spl_version}"
+    $spl_source_url = "${download_dir}/spl/${spl_installer_tar}"
 
-  ###
-  # Set resource defaults
-  ###
-  Exec { path => '/usr/local/bin:/usr/bin:/bin', }
+    $zfs_installer_tar = "zfs-${zfs_version}.tar.gz"
+    $zfs_installer_dir = "zfs-${zfs_version}"
+    $zfs_source_url = "${download_dir}/zfs/${zfs_installer_tar}"
 
-  ###
-  # Stage the installers
-  ###
+    ###
+    # Set resource defaults
+    ###
+    Exec { path => '/usr/local/bin:/usr/bin:/bin', }
 
-  staging::file { $spl_installer_tar :
-    source  => $spl_source_url,
-    timeout => $timeout,
-  }
+    ###
+    # Stage the installers
+    ###
 
-  staging::extract { $spl_installer_tar :
-    target  => "${staging::path}/zfsonlinux",
-    require => Staging::File[$spl_installer_tar],
-  }
+    staging::file { $spl_installer_tar :
+      source  => $spl_source_url,
+      timeout => $timeout,
+    }
 
-  staging::file { $zfs_installer_tar :
-    source  => $zfs_source_url,
-    timeout => $timeout,
-  }
+    staging::extract { $spl_installer_tar :
+      target  => "${staging::path}/zfsonlinux",
+      require => Staging::File[$spl_installer_tar],
+    }
 
-  staging::extract { $zfs_installer_tar :
-    target  => "${staging::path}/zfsonlinux",
-    require => Staging::File[$zfs_installer_tar],
-  }
+    staging::file { $zfs_installer_tar :
+      source  => $zfs_source_url,
+      timeout => $timeout,
+    }
 
-  ###
-  # Perform build of spl
-  ##
+    staging::extract { $zfs_installer_tar :
+      target  => "${staging::path}/zfsonlinux",
+      require => Staging::File[$zfs_installer_tar],
+    }
 
-  exec { 'configure spl':
-    command   => "${staging::path}/zfsonlinux/${spl_installer_dir}/configure",
-    cwd       => "${staging::path}/zfsonlinux/${spl_installer_dir}",
-    logoutput => on_failure,
-    #    creates   => "${staging::path}/zfsonlinux/${spl_installer_dir}/Makefile",
-    require   => Staging::Extract[$spl_installer_tar],
-  }
+    ###
+    # Perform build of spl
+    ##
 
-  exec { 'build spl':
-    command => 'make rpm',
-    cwd       => "${staging::path}/zfsonlinux/${spl_installer_dir}",
-    logoutput => on_failure,
-    require   => Exec['configure spl'],
-  }
+    exec { 'configure spl':
+      command   => "${staging::path}/zfsonlinux/${spl_installer_dir}/configure",
+      cwd       => "${staging::path}/zfsonlinux/${spl_installer_dir}",
+      logoutput => on_failure,
+      #    creates   => "${staging::path}/zfsonlinux/${spl_installer_dir}/Makefile",
+      require   => Staging::Extract[$spl_installer_tar],
+    }
 
-  exec { 'install spl rpms':
-    command   => "rpm -Uvh *.${::hardwareisa}.rpm",
-    cwd       => "${staging::path}/zfsonlinux/${spl_installer_dir}",
-    logoutput => on_failure,
-    require   => Exec['build spl'],
-    user      => 0,
-    group     => 0,
-  }
+    exec { 'build spl':
+      command => 'make rpm',
+      cwd       => "${staging::path}/zfsonlinux/${spl_installer_dir}",
+      logoutput => on_failure,
+      require   => Exec['configure spl'],
+    }
 
-  ###
-  # Perform build of zfs
-  ###
-  exec { 'configure zfs':
-    command   => "${staging::path}/zfsonlinux/${zfs_installer_dir}/configure",
-    cwd       => "${staging::path}/zfsonlinux/${zfs_installer_dir}",
-    logoutput => on_failure,
-    # creates   => "${staging::path}/zfsonlinux/${zfs_installer_dir}/Makefile",
-    require   => [
-      Staging::Extract[$zfs_installer_tar],
-      Exec['install spl rpms'],
-    ],
-  }
+    exec { 'install spl rpms':
+      command   => "rpm -Uvh *.${::hardwareisa}.rpm",
+      cwd       => "${staging::path}/zfsonlinux/${spl_installer_dir}",
+      logoutput => on_failure,
+      require   => Exec['build spl'],
+      user      => 0,
+      group     => 0,
+    }
 
-  exec { 'build zfs':
-    command => 'make rpm',
-    cwd       => "${staging::path}/zfsonlinux/${zfs_installer_dir}",
-    logoutput => on_failure,
-    require   => Exec['configure zfs'],
-  }
+    ###
+    # Perform build of zfs
+    ###
+    exec { 'configure zfs':
+      command   => "${staging::path}/zfsonlinux/${zfs_installer_dir}/configure",
+      cwd       => "${staging::path}/zfsonlinux/${zfs_installer_dir}",
+      logoutput => on_failure,
+      # creates   => "${staging::path}/zfsonlinux/${zfs_installer_dir}/Makefile",
+      require   => [
+        Staging::Extract[$zfs_installer_tar],
+        Exec['install spl rpms'],
+        ],
+    }
 
-  exec { 'install zfs rpms':
-    command   => "rpm -Uvh *.${::hardwareisa}.rpm",
-    cwd       => "${staging::path}/zfsonlinux/${zfs_installer_dir}",
-    logoutput => on_failure,
-    require   => Exec['build zfs'],
-    user      => 0,
-    group     => 0,
+    exec { 'build zfs':
+      command => 'make rpm',
+      cwd       => "${staging::path}/zfsonlinux/${zfs_installer_dir}",
+      logoutput => on_failure,
+      require   => Exec['configure zfs'],
+    }
+
+    exec { 'install zfs rpms':
+      command   => "rpm -Uvh *.${::hardwareisa}.rpm",
+      cwd       => "${staging::path}/zfsonlinux/${zfs_installer_dir}",
+      logoutput => on_failure,
+      require   => Exec['build zfs'],
+      user      => 0,
+      group     => 0,
+    }
+
   }
 
 }
