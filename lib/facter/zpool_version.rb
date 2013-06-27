f772808a5843046dc4db4a0cf6cf06f53151a43d
Geoff Davis <gadavis@ucsd.edu>
2013-06-27 11:54:10 -0700

    Vendor zpool_version from puppetlabs-facter
    Don't confine to Linux any more, just make sure we have the zpool
    command.

--- lib/facter/zpool_version.rb
+++ lib/facter/zpool_version.rb
@@ -1,14 +1,10 @@
 require 'facter'
+
 Facter.add(:zpool_version) do
-  confine :kernel => :linux
-  confine :zfsonlinux_present => :true
   setcode do
-    zpool_v = Facter::Util::Resolution.exec('zpool upgrade -v')
-    # VER  DESCRIPTION
-    # ---  --------------------------------------------------------
-    #  1   Initial ZFS version
-    # ...
-    #  28  Multiple vdev replacements
-    zpool_v.scan(/^ {1}(\d+)\s{2,3}.\w+/).flatten.last unless zpool_v.nil?
+    if Facter::Util::Resolution.which('zpool')
+      zpool_v = Facter::Util::Resolution.exec('zpool upgrade -v')
+      zpool_v.scan(/^ {1}(\d+)\s{2,3}.\w+/).flatten.last unless zpool_v.nil?
+    end
   end
 end
